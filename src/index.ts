0c06c76a402833f2f3f19fbd3f636ceda2b912fe
hpc <philipchanhk626@gmail.com>
2020-07-29 09:44:51 -0400

    created CustomMap class, extract googlemap into class

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import { User } from './User'
 import { Company } from './Company'
+import { CustomMap } from './CustomMap'
 
 const user = new User()
 const company = new Company()
 
-console.log(user)
-console.log(company)
+const customMap = new CustomMap('map')
 
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-})
